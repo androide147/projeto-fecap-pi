7f1196c2ff7c20a3dcbbe30ab14e10eb4bc90011
André de Sousa Pereira <and.1214.re@gmail.com>
2025-11-10 22:29:45 -0300

    Update vite.config.js

--- Projeto_Full_Stack/frontend/vite.config.js
+++ Projeto_Full_Stack/frontend/vite.config.js
@@ -1,7 +1,16 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // ADICIONE ESTE BLOCO DE CÓDIGO
+  preview: {
+    allowedHosts: [
+      // O seu domínio público do Railway
+      'protective-nature-production.up.railway.app' 
+    ]
+  }
 })
